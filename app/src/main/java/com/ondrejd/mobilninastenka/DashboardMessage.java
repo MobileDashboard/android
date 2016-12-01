305313f0cb5b3d4f144b7b61314cf97c177069b8
ondrejd <ondrejd@gmail.com>
2016-12-01 11:18:32 +0100

    Updated DashboardMessage class.

--- app/src/main/java/com/ondrejd/mobilninastenka/DashboardMessage.java
+++ app/src/main/java/com/ondrejd/mobilninastenka/DashboardMessage.java
@@ -4,76 +4,76 @@ package com.ondrejd.mobilninastenka;
  * Class that represents single dashboard message.
  */
 public class DashboardMessage {
-    public static String PRIORITY_EXTRA = "extra";
-    public static String PRIORITY_LOW = "low";
-    public static String PRIORITY_NORMAL = "normal";
+    public static int PRIORITY_EXTRA = 3;
+    public static int PRIORITY_LOW = 1;
+    public static int PRIORITY_NORMAL = 2;
 
+    private String boardHash;
+    private String boardName;
+    private int id;
     private String title;
-    private String text;
-    private String priority;
+    private String content;
+    //Timestamp with target data
+    private int timestamp;
+    private String link;
+    private int priority;
+    private boolean deleted;
+    //Timestamp when message will expire.
+    private int expiration;
     private String author;
-    private String created;
-    private String updated;
-
-    /**
-     * Construct blank dashboard message.
-     */
-    DashboardMessage() {
-        title = "";
-        text = "";
-        priority = PRIORITY_NORMAL;
-        author = "";
-        created = "";
-        updated = "";
-    }
+    //Timestamp when was message sent.
+    private int sent;
 
     /**
      * Construct dashboard message from a JSON string.
      * @param json JSON from which should be message constructed.
      */
     DashboardMessage(String json) {
-        //TODO Process given JSON string into the dashboard message.
+        //TODO Finish this!
     }
 
     /**
      * Construct dashboard message from given parameters
+     * @param boardHash Hash of message's board.
+     * @param boardName Name of message's board
+     * @param id Numeric identifier of the message
      * @param title Title of the message.
-     * @param text Text of the message.
+     * @param content Content of the message.
+     * @param timestamp Timestamp when...
+     * @param link Link attached to the message.
      * @param priority Message's priority.
+     * @param deleted TRUE if message is marked as deleted.
+     * @param expiration Timestamp when the message will expire.
      * @param author Author of the message.
-     * @param created Datetime when was message created.
-     * @param updated Datetime when was message updated.
+     * @param sent Timestamp when was message sent.
      */
-    DashboardMessage(String title, String text, String priority, String author, String created, String updated) {
+    DashboardMessage(String boardHash, String boardName, int id, String title, String content,
+                     int timestamp, String link, int priority, boolean deleted, int expiration,
+                     String author, int sent) {
+        this.boardHash = boardHash;
+        this.boardName = boardName;
+        this.id = id;
         this.title = title;
-        this.text = text;
+        this.content = content;
+        this.timestamp = timestamp;
+        this.link = link;
         this.priority = priority;
+        this.deleted = deleted;
+        this.expiration = expiration;
         this.author = author;
-        this.created = created;
-        this.updated = updated;
+        this.sent = sent;
     }
 
+    public String getBoardHash() { return boardHash; }
+    public String getBoardName() { return boardName; }
+    public int getId() { return id; }
     public String getTitle() { return title; }
-    public void setTitle(String val) { title = val; }
-
-    public String getText() { return text; }
-    public void setText(String val) { text = val; }
-
-    public String getPriority() { return priority; }
-    public void setPriority(String val) {
-        if (val == PRIORITY_EXTRA || val == PRIORITY_LOW || val == PRIORITY_NORMAL) {
-            priority = val;
-        } else {
-            priority = PRIORITY_NORMAL;
-        }
-    }
-
+    public String getContent() { return content; }
+    public int getTimestamp() { return timestamp; }
+    public String getLink() { return link; }
+    public int getPriority() { return priority; }
+    public boolean getDeleted() { return deleted; }
+    public int getExpiration() { return expiration; }
     public String getAuthor() { return author; }
-    public void setAuthor(String val) { author = val; }
-
-    public String getCreated() { return created; }
-    public void setCreated(String val) { created = val; }
-
-    public String getUpdated() { return updated; }
-    public void setUpdated(String val) { updated = val; }
+    public int getSent() { return sent; }
 }
